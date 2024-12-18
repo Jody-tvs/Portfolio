8e0466699f5ca923262c194ad48a6fd85badb4e1
Carole Tavares <jtavares.mcd@gmail.com>
2024-12-18 11:07:52 +0100

    Ajout des modifications

--- scripts/app.js
+++ scripts/app.js
@@ -6,3 +6,23 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     })
   })
 })
+
+//gère l'effet du scroll
+document.addEventListener("DOMContentLoaded", () => {
+  const fadeInContents = document.querySelectorAll(".fade-in-content")
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible")
+      } else {
+        //retire l'effet quand l'élément sort de la vue
+        entry.target.classList.remove("visible")
+      }
+    })
+  }, {
+    threshold: 0.2 //active quand 20% du contenu est visible
+  })
+
+  fadeInContents.forEach(content => observer.observe(content))
+})
