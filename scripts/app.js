c9a34a078be9293d93ae71edebbcf507627dec9b
tavares.jody <tavares.jody.3wa.io>
2025-01-23 12:40:25 +0100

    changement des realisations html en JS

--- scripts/app.js
+++ scripts/app.js
@@ -8,7 +8,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   })
 })
 
-//gère l'effet du scroll
+//gère l'effet de fade-in au scroll
 document.addEventListener("DOMContentLoaded", () => {
   const fadeInContents = document.querySelectorAll(".fade-in-content")
 
@@ -27,6 +27,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
   fadeInContents.forEach(content => observer.observe(content))
 })
+
+//effet d'écriture typewriting sur le titre principal
+function typeWriter() {
+  if (i < text.length) {
+    textElement.textContent += text.charAt(i)
+    i++
+    setTimeout(typeWriter, 100)
+  }
+}
+window.addEventListener("load", typeWriter) //l'effet se lance au chargement de la page
 
 const textElement = document.querySelector("#home h1")
 const text = textElement.textContent
@@ -61,12 +71,40 @@ skills.forEach(skill => {
   skillsContainer.appendChild(skillDiv)
 })
 
-//effet typewriting sur le titre
-function typeWriter() {
-  if (i < text.length) {
-    textElement.textContent += text.charAt(i)
-    i++
-    setTimeout(typeWriter, 100)
-  }
-}
-window.addEventListener("load", typeWriter)
+//projets
+const projects = [
+  {
+    title: "Folie d'Armes",
+    description: "Site e-commerce créé avec JavaScript, SASS et HTML.",
+    image: "assets/folie.png",
+    link: "projets/projet1.html",
+  },
+  {
+    title: "Manhattan Restaurant",
+    description: "Site vitrine créé avec HTML et CSS.",
+    image: "assets/manhattan.png",
+    link: "projets/projet2.html",
+  },
+  {
+    title: "Le monde à portée",
+    description: "Blog de voyage créé avec HTML, CSS et JavaScript.",
+    image: "assets/monde.png",
+    link: "projets/projet3.html",
+  },
+]
+
+const projectsGrid = document.getElementById("projects-grid")
+
+projects.forEach(project => {
+  const projectDiv = document.createElement("div")
+  projectDiv.classList.add("project")
+
+  projectDiv.innerHTML = `
+    <h3>${project.title}</h3>
+    <img src="${project.image}" alt="${project.title}" class="photo-style">
+    <p>${project.description}</p>
+    <a href="${project.link}" class="btn-primary">Détails du projet</a>
+  `
+
+  projectsGrid.appendChild(projectDiv)
+})
